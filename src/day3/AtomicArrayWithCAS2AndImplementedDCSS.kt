464967ab99dc4d129d9dc175e21e1e83c7642e59
Vladislav Grinin <vladislav.grinin@jetbrains.com>
2025-07-24 19:12:34 +0200

    complete AtomicArrayWithCAS2AndImplementedDCSS

--- src/day3/AtomicArrayWithCAS2AndImplementedDCSS.kt
+++ src/day3/AtomicArrayWithCAS2AndImplementedDCSS.kt
@@ -18,7 +18,21 @@ class AtomicArrayWithCAS2AndImplementedDCSS<E : Any>(size: Int, initialValue: E)
 
     fun get(index: Int): E {
         // TODO: Copy the implementation from `AtomicArrayWithCAS2Simplified`
-        TODO("Implement me")
+        when (val value = array[index]) {
+            is AtomicArrayWithCAS2AndImplementedDCSS<*>.CAS2Descriptor -> {
+                val previousValue = if (value.index1 == index) value.expected1 else value.expected2
+                val newValue = if (value.index1 == index) value.update1 else value.update2
+
+                return when (value.status.get()) {
+                    UNDECIDED, FAILED -> previousValue as E
+                    SUCCESS -> newValue as E
+                    else -> error("Unexpected status: ${value.status.get()}")
+                }
+            }
+            else -> {
+                return value as E
+            }
+        }
     }
 
     fun cas2(
@@ -26,9 +40,18 @@ class AtomicArrayWithCAS2AndImplementedDCSS<E : Any>(size: Int, initialValue: E)
         index2: Int, expected2: E, update2: E
     ): Boolean {
         require(index1 != index2) { "The indices should be different" }
+
+        val first = if (index1 < index2) index1 else index2
+        val firstExpected = if (first == index1) expected1 else expected2
+        val firstUpdate = if (first == index1) update1 else update2
+
+        val second = if (first == index1) index2 else index1
+        val secondExpected = if (second == index1) expected1 else expected2
+        val secondUpdate = if (second == index1) update1 else update2
+
         val descriptor = CAS2Descriptor(
-            index1 = index1, expected1 = expected1, update1 = update1,
-            index2 = index2, expected2 = expected2, update2 = update2
+            index1 = first, expected1 = firstExpected, update1 = firstUpdate,
+            index2 = second, expected2 = secondExpected, update2 = secondUpdate
         )
         descriptor.apply()
         return descriptor.status.get() === SUCCESS
@@ -54,12 +77,32 @@ class AtomicArrayWithCAS2AndImplementedDCSS<E : Any>(size: Int, initialValue: E)
             // TODO: Install this descriptor to the cells,
             // TODO: returning `true` on success, and `false`
             // TODO: if one of the cells contained an unexpected value.
-            TODO("Implement me!")
+            return install(index1, expected1) && install(index2, expected2)
+        }
+
+        private fun install(index: Int, expected: E): Boolean {
+            while (true) {
+                if (status.get() != UNDECIDED) {
+                    return false
+                }
+
+                when (val currentValue = array[index]) {
+                    this -> return true
+                    is AtomicArrayWithCAS2AndImplementedDCSS<*>.CAS2Descriptor -> currentValue.apply()
+                    expected -> {
+                        if (dcss(index, currentValue, this, status, UNDECIDED)) {
+                            return true
+                        }
+                    }
+                    else -> return false
+                }
+            }
         }
 
         private fun applyLogically(success: Boolean) {
             // TODO: Apply this CAS2 operation logically
             // TODO: by updating the descriptor status.
+            status.compareAndSet(UNDECIDED, if (success) SUCCESS else FAILED)
         }
 
         private fun applyPhysically() {
@@ -67,6 +110,17 @@ class AtomicArrayWithCAS2AndImplementedDCSS<E : Any>(size: Int, initialValue: E)
             // TODO: by updating the cells to either
             // TODO: update values (on success)
             // TODO: or back to expected values (on failure).
+            when (status.get()) {
+                SUCCESS -> {
+                    array.compareAndSet(index1, this, update1)
+                    array.compareAndSet(index2, this, update2)
+                }
+                UNDECIDED, FAILED -> {
+                    array.compareAndSet(index1, this, expected1)
+                    array.compareAndSet(index2, this, expected2)
+                }
+                else -> error("Unexpected status: ${status.get()}")
+            }
         }
     }
 
