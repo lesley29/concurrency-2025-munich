84747af1bd1354334287f5435b7ba33b9081a510
Vladislav Grinin <vladislav.grinin@jetbrains.com>
2025-07-24 11:48:27 +0200

    complete AtomicArrayWithCAS2OnLockedState

--- src/day3/AtomicArrayWithCAS2OnLockedState.kt
+++ src/day3/AtomicArrayWithCAS2OnLockedState.kt
@@ -2,6 +2,7 @@ package day3
 
 import java.util.concurrent.atomic.*
 import java.util.concurrent.locks.*
+import kotlin.concurrent.withLock
 
 // This implementation never stores `null` values.
 class AtomicArrayWithCAS2OnLockedState<E : Any>(size: Int, initialValue: E) {
@@ -16,8 +17,57 @@ class AtomicArrayWithCAS2OnLockedState<E : Any>(size: Int, initialValue: E) {
 
     fun get(index: Int): E {
         // TODO: Cover the case when the cell state is LOCKED.
-        return array[index] as E
+        while (true) {
+            val value = array[index]
+            if (value != LOCKED) {
+                @Suppress("UNCHECKED_CAST")
+                return value as E
+            }
+        }
     }
+
+//    fun cas2(
+//        index1: Int, expected1: E, update1: E,
+//        index2: Int, expected2: E, update2: E
+//    ): Boolean {
+//        require(index1 != index2) { "The indices should be different" }
+//        // TODO: Make me thread-safe by "locking" the cells
+//        // TODO: via atomically changing their states to LOCKED.
+//        val first = if (index1 < index2) index1 else index2
+//        val firstExpected = if (first == index1) expected1 else expected2
+//        val firstUpdate = if (first == index1) update1 else update2
+//
+//        val second = if (first == index1) index2 else index1
+//        val secondExpected = if (second == index1) expected1 else expected2
+//        val secondUpdate = if (second == index1) update1 else update2
+//
+//        while (true) {
+//            if (array[first] == LOCKED) {
+//                continue
+//            }
+//
+//            if (!array.compareAndSet(first, firstExpected, LOCKED)) {
+//                return false
+//            }
+//
+//            while (true) {
+//                if (array[second] == LOCKED) {
+//                    continue
+//                }
+//
+//                if (!array.compareAndSet(second, secondExpected, LOCKED)) {
+//                    array.compareAndSet(first, LOCKED, firstExpected)
+//                    return false
+//                }
+//
+//                break
+//            }
+//
+//            array.set(first, firstUpdate)
+//            array.set(second, secondUpdate)
+//            return true
+//        }
+//    }
 
     fun cas2(
         index1: Int, expected1: E, update1: E,
@@ -26,12 +76,41 @@ class AtomicArrayWithCAS2OnLockedState<E : Any>(size: Int, initialValue: E) {
         require(index1 != index2) { "The indices should be different" }
         // TODO: Make me thread-safe by "locking" the cells
         // TODO: via atomically changing their states to LOCKED.
-        if (array[index1] === expected1 && array[index2] === expected2) {
-            array.set(index1, update1)
-            array.set(index2, update2)
+        val first = if (index1 < index2) index1 else index2
+        val firstExpected = if (first == index1) expected1 else expected2
+        val firstUpdate = if (first == index1) update1 else update2
+
+        val second = if (first == index1) index2 else index1
+        val secondExpected = if (second == index1) expected1 else expected2
+        val secondUpdate = if (second == index1) update1 else update2
+
+        while (true) {
+            if (!lockCell(first, firstExpected)) {
+                return false
+            }
+
+            if (!lockCell(second, secondExpected)) {
+                array.compareAndSet(first, LOCKED, firstExpected)
+                return false
+            }
+
+            array.set(first, firstUpdate)
+            array.set(second, secondUpdate)
             return true
-        } else {
-            return false
+        }
+    }
+
+    private fun lockCell(index: Int, expectedValue: E): Boolean {
+        while (true) {
+            when (array[index]) {
+                expectedValue -> {
+                    if (array.compareAndSet(index, expectedValue, LOCKED)) {
+                        return true
+                    }
+                }
+                LOCKED -> continue
+                else -> return false
+            }
         }
     }
 }
